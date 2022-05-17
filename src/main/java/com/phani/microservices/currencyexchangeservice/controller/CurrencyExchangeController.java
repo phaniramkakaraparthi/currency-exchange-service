5d792defa1b7a411fd8b0ec5f43ebd9be3968acb
phaniramkakaraparthi <22965815+phaniramkakaraparthi@users.noreply.github.com>
2022-05-17 18:24:25 +0530

    added config for docker image creation

--- src/main/java/com/phani/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
+++ src/main/java/com/phani/microservices/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.phani.microservices.currencyexchangeservice.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,11 +17,15 @@ public class CurrencyExchangeController {
 	@Autowired
 	private CurrencyExchangeRepository repository;
 	
+	Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+	
 	@Autowired
 	private Environment environment;
 	
 	@GetMapping("currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+		
+		logger.info("retrieveExchangeValue called with {} to {}",from ,to);
 		
 		CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
 		
